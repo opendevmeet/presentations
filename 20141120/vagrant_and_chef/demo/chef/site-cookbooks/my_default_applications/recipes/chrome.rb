0083d2e5e3bf4b1ff4fba4c02b6dc510a0b95a47
Stefan Morgenthaler <stefan+dev@morgenthaler.at>
2014-11-20 12:12:16 +0100

    Don’t mess with Voodoo

---
+++ 20141120/vagrant_and_chef/demo/chef/site-cookbooks/my_default_applications/recipes/chrome.rb
@@ -0,0 +1,6 @@
+dmg_package 'Google Chrome' do
+  dmg_name 'googlechrome'
+  source   'https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg'
+  checksum '5cc869c2e8b7e42a61677739411c097664745c5c0d4886832cbbbe6f8b6dadba'
+  action   :install
+end
